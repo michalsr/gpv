0157e0a045ed48a1a8ff6dbddfc91298b05cb05f
chrisc <chrisc@allenai.org>
2021-07-27 13:51:36 -0700

    Commit accidently excluded models

---
+++ exp/ours/models/layers.py
@@ -0,0 +1,366 @@
+import torch
+import torchvision.ops
+import transformers.models.deberta.modeling_deberta
+from allennlp.common import Registrable, Params
+from omegaconf import OmegaConf
+from torch import nn
+from torch.nn import functional as F
+from transformers import AutoTokenizer
+from transformers.models.t5.modeling_t5 import T5LayerNorm
+
+from exp.gpv.models.backbone import build_backbone, Backbone
+from exp.gpv.models.position_encoding import build_position_encoding, PositionEmbeddingLearned, \
+  PositionEmbeddingSine
+from exp.ours import file_paths
+from exp.ours.data.source_data import ID_TO_COCO_CATEGORY
+from exp.ours.util import our_utils, py_utils
+from exp.ours.util.our_utils import replace_params_with_buffers
+from exp.ours.util.to_params import to_params
+from utils.detr_misc import NestedTensor
+
+
+class Layer(Registrable, nn.Module):
+  pass
+
+
+def get_fn(fn_name):
+  if fn_name == "relu":
+    return torch.relu
+  else:
+    raise NotImplementedError()
+
+
+@Layer.register("linear")
+class Linear(nn.Linear, Layer):
+  def to_params(self):
+    return dict(
+      in_features=self.in_features,
+      out_features=self.out_features,
+      bias=self.bias is not None,
+    )
+
+@Layer.register("relu")
+class Relu(nn.ReLU, Layer):
+  pass
+
+
+@Layer.register("null")
+class NullLayer(Layer):
+  def forward(self, x):
+    return x
+
+
+@Layer.register("sequential")
+class Sequence(nn.Sequential, Layer):
+
+  @classmethod
+  def from_params(
+        cls,
+        params: Params,
+        constructor_to_call = None,
+        constructor_to_inspect = None,
+        **extras,
+  ):
+    return Sequence(*[Layer.from_params(x) for x in params["args"]])
+
+  def to_params(self):
+    return dict(args=[to_params(x, Layer) for x in self])
+
+
+@Layer.register("embedding-simplex")
+class EmbeddingSimplex(Layer):
+  def __init__(self, n_in, n_embeddings, n_out, init="zero",
+               pos_init=1, neg_init=-1, lin_factor=1.0):
+    super().__init__()
+    self.n_in = n_in
+    self.n_embeddings = n_embeddings
+    self.n_out = n_out
+    self.init = init
+    self.pos_init = pos_init
+    self.neg_init = neg_init
+    self.lin_factor = lin_factor
+
+    self.embed_predictor = nn.Linear(self.n_in, self.n_embeddings)
+    self.embed_predictor.weight.data[:] = 0.0
+    self.embed_predictor.bias.data[:] = 0
+    self.to_add = nn.Linear(self.n_in, self.n_out)
+    self.to_add.bias.data[:] = 0
+    self.to_add.weight.data[:] = 0
+    if self.init == "t5-coco-categories":
+      tok = AutoTokenizer.from_pretrained("t5-base")
+      ids = set()
+      ids.add(tok.eos_token_id)
+      for word in ID_TO_COCO_CATEGORY.values():
+        ids.update(tok.encode(word))
+      self.embed_predictor.bias.data[:] = self.neg_init
+      self.embed_predictor.bias.data[list(ids)] = self.pos_init
+    elif self.init != "zero":
+      raise ValueError()
+
+  def forward(self, features, embeddings):
+    embed_score = F.softmax(self.embed_predictor(features), -1)
+    embed_features = torch.matmul(embed_score, embeddings.weight[:self.n_embeddings])
+    return embed_features + self.to_add(features) * self.lin_factor
+
+
+@Layer.register("linear-objectness")
+class LinearObjectness(Layer):
+
+  def __init__(self, n_in):
+    super().__init__()
+    self.n_in = n_in
+    self.norm = T5LayerNorm(self.n_in)
+    self.rel = nn.Linear(self.n_in, 1)
+
+  def forward(self, encoder, objectness, boxes):
+    n_images = boxes.size(1)
+    image_rel = self.rel(self.norm(encoder[:, :n_images]))
+    return image_rel.squeeze(-1)
+
+
+@Layer.register("sum-with-objectness-v3")
+class SumWithObjectness(Layer):
+
+  def __init__(self, n_in, objectness_factor=False, multi_class_mode="any-object"):
+    super().__init__()
+    self.n_in = n_in
+    self.multi_class_mode = multi_class_mode
+    self.objectness_factor = objectness_factor
+    self.norm = T5LayerNorm(self.n_in)
+    self.rel = nn.Linear(self.n_in, 1 + objectness_factor)
+    # Initialize so it just uses objectness at the start
+    self.rel.weight.data[:] = 0
+    if objectness_factor:
+      self.rel.bias.data[1] = 1.0
+
+  def forward(self, encoder, objectness, boxes):
+    n_images = boxes.size(1)
+    image_rel = self.rel(self.norm(encoder[:, :n_images]))
+    if self.objectness_factor:
+      image_rel, factor = torch.split(image_rel, [1, 1], -1)
+      factor = factor.squeeze(-1)
+    else:
+      factor = None
+    image_rel = image_rel.squeeze(-1)
+
+    if len(objectness.size()) == 3:
+      if self.multi_class_mode == "any-object":
+        non_object_lp = F.log_softmax(objectness, -1)[:, :, -1]
+        object_lp = torch.log1p(-torch.exp(non_object_lp))
+      elif self.multi_class_mode == "max-object":
+        object_lp = torch.max(F.log_softmax(objectness, -1)[:, :, :-1], 2)[0]
+        non_object_lp = torch.log1p(-torch.exp(object_lp))
+      else:
+        raise NotImplementedError()
+    else:
+      object_lp = objectness
+      non_object_lp = torch.log1p(-torch.exp(object_lp))
+
+    objectness = object_lp - non_object_lp
+    if factor is not None:
+      objectness = objectness * factor
+    return image_rel + objectness
+
+
+@Layer.register("sum-with-objectness-v2")
+class SumWithObjectnessV2(Layer):
+
+  def __init__(self, n_in, mode="any-object", objectness_factor=False):
+    super().__init__()
+    self.n_in = n_in
+    self.mode = mode
+    self.objectness_factor = objectness_factor
+    self.norm = T5LayerNorm(self.n_in)
+    self.rel = nn.Linear(self.n_in, 1 + objectness_factor)
+    # Initialize so it just uses objectness at the start
+    self.rel.weight.data[:] = 0
+    if objectness_factor:
+      self.rel.bias.data[1] = 1.0
+
+  def forward(self, encoder, objectness, boxes):
+    n_images = boxes.size(1)
+    image_rel = self.rel(self.norm(encoder[:, :n_images]))
+    if self.objectness_factor:
+      image_rel, factor = torch.split(image_rel, [1, 1], -1)
+      factor = factor.squeeze(-1)
+    else:
+      factor = None
+    image_rel = image_rel.squeeze(-1)
+
+    if self.mode == "any-object":
+      non_object_lp = F.log_softmax(objectness, -1)[:, :, -1]
+      object_lp = torch.log1p(-torch.exp(non_object_lp))
+    elif self.mode == "none":
+      return image_rel
+    elif self.mode == "max-object":
+      object_lp = torch.max(F.log_softmax(objectness, -1)[:, :, :-1], 2)[0]
+      non_object_lp = torch.log1p(-torch.exp(object_lp))
+    else:
+      raise NotImplementedError()
+
+    objectness = object_lp - non_object_lp
+    if factor is not None:
+      objectness = objectness * factor
+    return image_rel + objectness
+
+
+@Layer.register("image-rel-decode-v1")
+class ImageRelDecoderV1(Layer):
+
+  def __init__(self, n_in, hidden_dim, n_heads, dim_feedforward,
+               n_layers, box_embedder: Layer, dropout=0.1):
+    super().__init__()
+    self.n_heads = n_heads
+    self.n_in = n_in
+    self.dim_feedforward = dim_feedforward
+    self.hidden_dim = hidden_dim
+    self.box_embedder = box_embedder
+    self.dropout = dropout
+    self.n_layers = n_layers
+
+    self.norm = T5LayerNorm(n_in)
+    self.join = nn.Linear(n_in, hidden_dim)
+    self.cls = nn.Linear(hidden_dim, 1)
+    layers = [
+      nn.TransformerEncoderLayer(hidden_dim, n_heads, dim_feedforward, dropout)
+      for _ in range(n_layers)
+    ]
+    self.layers = nn.Sequential(*layers)
+
+  def forward(self, encoder, objectness, boxes):
+    batch, n_images = objectness.size()[:2]
+    embed = self.join(self.norm(encoder[:, :n_images]))
+    box_embed = self.box_embedder(boxes)
+    for l in self.layers:
+      embed = l(embed + box_embed)
+    return self.cls(embed).squeeze(-1)
+
+
+@Layer.register("predict-objectness")
+class PredictObjectness(Layer):
+
+  def __init__(self, n_in, n_classes, w):
+    super().__init__()
+    self.n_classes = n_classes
+    self.n_in = n_in
+    self.w = w
+    self.cls = nn.Linear(n_in, n_classes)
+
+  def forward(self, box_embed, image_features):
+    pred = self.cls(box_embed)
+    lp = F.log_softmax(pred, -1)
+    total_lp = lp*F.softmax(image_features.objectness, -1)
+    loss = -total_lp.sum(-1).mean(1).mean()
+    return loss*self.w, dict(object_loss=loss)
+
+
+@Layer.register("basic-box-embedder")
+class BasicBoxEmbedder(Layer):
+  def forward(self, boxes: torch.Tensor):
+    cx, cy, w, h = [x.squeeze(-1) for x in boxes.split([1, 1, 1, 1], -1)]
+    return torch.stack([
+      cx,
+      cy,
+      w,
+      h,
+      w*h
+    ], -1)
+
+
+@Layer.register("with-log")
+class NonLinearCoordinateEncoder(Layer):
+  def __init__(self, log_alpha, coordinate_layer: Layer=None):
+    super().__init__()
+    self.log_alpha = log_alpha
+    self.coordinate_layer = coordinate_layer
+    self.register_buffer("_log_alpha", torch.as_tensor(log_alpha, dtype=torch.float32))
+
+  def forward(self, x):
+    batch, seq, dim = x.size()
+    alpha = self._log_alpha.view(1, 1, 1, -1)
+    logs = torch.cat([
+      torch.log(x.unsqueeze(-1) + alpha),
+      torch.log1p(- x.unsqueeze(-1) + alpha),
+    ], -1)
+    logs = logs.view(batch, seq, -1)
+    logs = torch.cat([x, logs], -1)
+    if torch.any(torch.isnan(logs)):
+      print(x.min(), x.max())
+      raise ValueError()
+    if self.coordinate_layer:
+      logs = self.coordinate_layer(logs)
+    return logs
+
+
+@Layer.register("layer-norm")
+class LayerNorm(Layer):
+  def __init__(self, eps=1e-5):
+    super().__init__()
+    self.eps = eps
+
+  def forward(self, x):
+    return F.layer_norm(x, (x.size(-1),))
+
+
+@Layer.register("detr-backbone")
+class DetrBackbone(Layer):
+  def __init__(self, backbone="resnet50", dilation: bool=False, frozenbatchnorm=True,
+               freeze=None):
+    super().__init__()
+    self.backbone = backbone
+    self.dilation = dilation
+    self.frozenbatchnorm = frozenbatchnorm
+    self.model = Backbone(backbone, True, False, dilation, frozenbatchnorm)
+    self.freeze = freeze
+    if self.freeze == "all":
+      for p in self.parameters():
+        p.requires_grad = False
+    elif self.freeze is not None and self.freeze != "none":
+      raise NotImplementedError(self.freeze)
+
+  def forward(self, x) -> NestedTensor:
+    return self.model(x)['0']
+
+
+@Layer.register("pretrained-detr-backbone")
+class PretrainedDetrBackbone(DetrBackbone):
+  def __init__(self, detr_model, freeze=None):
+    super().__init__(freeze=freeze)
+    self.detr_model = detr_model
+    state_dict = torch.load(file_paths.PRETRAINED_DETR_MODELS[detr_model], map_location="cpu")["model"]
+    tmp = py_utils.extract_module_from_state_dict(state_dict, "backbone.0")
+    self.model.load_state_dict(tmp)
+
+
+class AddSinePositionalEmbedding(Layer):
+  def __init__(self, num_features=128, temperature=10000, normalize=False):
+    super().__init__()
+    self.num_features = num_features
+    self.temperature = temperature
+    self.normalize = normalize
+    self.pos_embedder = PositionEmbeddingSine(num_features, temperature, normalize=normalize)
+
+  def forward(self, x: NestedTensor):
+    pos = self.pos_embedder(x)
+    return NestedTensor(x+pos, x.mask)
+
+
+@Layer.register("two-layer-ff")
+class TwoLayerFF(Layer):
+
+  def __init__(self, input_dim, hidden_dim, out_dim, fn="relu", dropout=0.0):
+    super().__init__()
+    self.out_dim = out_dim
+    self.dropout = dropout
+    self.input_dim = input_dim
+    self.hidden_dim = hidden_dim
+    self.fn = fn
+    self._fn = get_fn(fn)
+    self.lin = nn.Linear(input_dim, hidden_dim)
+    self.out = nn.Linear(hidden_dim, self.out_dim)
+
+  def forward(self, x):
+    out = self._fn(self.lin(x))
+    out = torch.dropout(out, self.dropout, self.training)
+    return self.out(out)
+
---
+++ exp/ours/models/t5_gpv.py
@@ -0,0 +1,395 @@
+import logging
+from typing import Union, Tuple, Dict, Optional
+
+import torch
+import torchvision.ops
+from allennlp.common import Params
+from transformers import AutoTokenizer, AutoConfig
+
+from exp.ours.data.source_data import ID_TO_COCO_CATEGORY
+from exp.ours.models.model import GPVModel, build_per_example_output
+from exp.ours.models.gpv1_preprocessing import Gpv1Preprocessor
+from exp.ours.image_featurizer.image_featurizer import ImageFeatureExtractor, ImageRegionFeatures
+from exp.ours.models.layers import Layer, Linear
+from exp.ours.models.losses import GPVLoss, BasicGPVLoss
+import numpy as np
+
+from exp.ours.models.model_utils import CollateWithTokenizer
+from exp.ours.models.t5_custom import OurT5ForConditionalGeneration
+from exp.ours.train.runner import BeamSearchSpec
+from exp.ours.util import our_utils
+from exp.ours.util.nlp_utils import encode_with_cache, t5_initialize_decoding
+from torch import nn
+from torch.nn import functional as F
+from exp.ours.util.to_params import to_params
+
+
+def _fix_old_params(params: Params):
+  if "loss" not in params:
+    params["loss"] = to_params(BasicGPVLoss(
+      1.0, None, None, None, None, None, None, sum_seq_tokens=False), GPVLoss)
+
+  if params.pop("aux_obj_cls") is not None:
+    raise ValueError()
+  if not params.pop("image_first"):
+    raise ValueError()
+  if "sort_image_features" in params:
+    if params.pop("sort_image_features") is not None:
+      raise ValueError()
+  if params.pop("use_cached_features"):
+    raise ValueError()
+
+  if "detr_joiner" in params:
+    params["image_joiner"] = params.pop("detr_joiner")
+  else:
+    params["image_joiner"] = dict(
+      type="linear", in_features=2304, out_features=768)
+  freeze = params.pop("freeze_detr")
+  if freeze is True:
+    freeze = "all-but-relevance"
+  params["image_feature_extractor"] = {'type': 'detr', 'freeze': freeze}
+
+
+def _fix_old_state_dict(state_dict: Dict[str, torch.Tensor]):
+  for k in list(state_dict):
+    if k.startswith("detr_joiner."):
+      v = state_dict[k]
+      del state_dict[k]
+      state_dict["image_joiner." + k[len("detr_joiner."):]] = v
+
+    if k.startswith("detr."):
+      v = state_dict[k]
+      del state_dict[k]
+      state_dict["image_feature_extractor." + k] = v
+
+
+@GPVModel.register("t5-gpv")
+class T5GPV(GPVModel):
+  IMAGE_SEP = "[IMAGE]"
+
+  @classmethod
+  def from_params(
+    cls,
+    params: Params,
+    **kwargs,
+  ):
+    if "image_feature_extractor" not in params:
+      _fix_old_params(params)
+    if "box_aux_loss" in params:
+      assert params.pop("box_aux_loss") is None
+    return super().from_params(params, **kwargs)
+
+  def __init__(
+      self,
+      t5_model_name: str,
+      loss: GPVLoss,
+      image_feature_extractor: ImageFeatureExtractor,
+      image_joiner: Layer,
+      image_relevance: Optional[Layer]=None,
+      initialize_t5=True, pre_tokenize=True,
+      query_box=None,
+      predict_trailing_pad_tokens=False,
+      image_positional_bias="zero",
+      image_seperator=False,
+      freeze_embed=False,
+      initialize_joiner="coco",
+      nms: float=0.0,
+  ):
+    super().__init__()
+    if freeze_embed and image_seperator:
+      raise NotImplementedError()
+    self.image_relevance = image_relevance
+    self.freeze_embed = freeze_embed
+    self.t5_model_name = t5_model_name
+    self.loss = loss
+    self.image_feature_extractor = image_feature_extractor
+    self.initialize_t5 = initialize_t5
+    self.predict_trailing_pad_tokens = predict_trailing_pad_tokens
+    self.image_positional_bias = image_positional_bias
+    self.pre_tokenize = pre_tokenize
+    self.image_seperator = image_seperator
+    self.initialize_joiner = initialize_joiner
+    self.nms = nms
+    self.initialize_t5 = initialize_t5
+    self.query_box = query_box
+
+    self.tokenizer = AutoTokenizer.from_pretrained(self.t5_model_name)
+    self.tokenizer_cache = {}
+
+    if self.image_seperator:
+      self.tokenizer.add_tokens([self.IMAGE_SEP])
+      image_sep_id = self.tokenizer.convert_tokens_to_ids([self.IMAGE_SEP])
+      assert len(image_sep_id) == 1
+      self._image_sep_id = image_sep_id[0]
+    else:
+      self._image_sep_id = None
+
+    self.image_joiner = image_joiner
+
+    self.preprocesser = Gpv1Preprocessor()
+    self.preprocesser.init(self._pre_tokenize)
+
+    self.model = None
+    self.obj_cls = None
+
+  def _pre_tokenize(self, text: str) -> Union[str, np.ndarray]:
+    if self.pre_tokenize:
+      return np.array(encode_with_cache(text, self.tokenizer, self.tokenizer_cache))
+    else:
+      return text
+
+  def initialize(self):
+    if self.initialize_t5:
+      logging.info(f"Loading pre-trained LM {self.t5_model_name}")
+      self.model: OurT5ForConditionalGeneration = OurT5ForConditionalGeneration.from_pretrained(self.t5_model_name)
+    else:
+      config = AutoConfig.from_pretrained(self.t5_model_name)
+      self.model = OurT5ForConditionalGeneration(config)
+
+    self._init_non_pretrained()
+
+    if self.initialize_joiner:
+      if self.initialize_joiner == "coco":
+        words = ID_TO_COCO_CATEGORY.values()
+      else:
+        raise NotImplementedError()
+      logging.info("Initializing joiner bias with mean embeddings")
+      if isinstance(self.image_joiner, Linear):
+        all_tokens = set()
+        for cat in words:
+          all_tokens.update(encode_with_cache(cat, self.tokenizer, self.tokenizer_cache))
+        all_tokens = torch.as_tensor(list(all_tokens), dtype=torch.long)
+        self.image_joiner.bias.data[:] = self.model.shared(all_tokens).mean(0)
+      else:
+        raise NotImplementedError()
+
+  def _init_non_pretrained(self):
+    t5_dim = self.model.config.d_model
+    n_heads = self.model.config.num_heads
+
+    if self.image_relevance is None:
+      self.obj_cls = nn.Linear(t5_dim, 2)
+
+    if self.query_box is not None:
+      self.query_embedding = nn.Parameter(torch.zeros(t5_dim).uniform_(-0.05, 0.05))
+
+    if self.image_positional_bias == "learned":
+      self.learned_image_text_bias = nn.Parameter(torch.zeros(n_heads,))
+      self.learned_image_image_bias = nn.Parameter(torch.zeros(n_heads,))
+      self.learned_text_image_bias = nn.Parameter(torch.zeros(n_heads,))
+    else:
+      self.learned_image_text_bias = None
+      self.learned_image_image_bias = None
+      self.learned_text_image_bias = None
+
+    if self.freeze_embed:
+      self.model.get_input_embeddings().weight.requires_grad = False
+      self.model.get_output_embeddings().weight.requires_grad = False
+
+  def _load_from_state_dict(self, state_dict, prefix, *args, **kwargs):
+    if self.model is None:
+      config = AutoConfig.from_pretrained(self.t5_model_name)
+      self.model = OurT5ForConditionalGeneration(config)
+
+    self._init_non_pretrained()
+
+    if any(k.startswith("detr_joiner.") for k in state_dict):
+      _fix_old_state_dict(state_dict)
+
+    super()._load_from_state_dict(state_dict, prefix, *args, **kwargs)
+
+  def preprocess_example_train(self, example):
+    return self.preprocesser.preprocess_example(example, True, self.query_box)
+
+  def preprocess_example(self, example, is_train=False):
+    return self.preprocesser.preprocess_example(example, is_train, self.query_box)
+
+  def get_collate(self, is_train=False):
+    n_pos = self.model.config.n_positions
+    return CollateWithTokenizer(
+      self.tokenizer, self.image_feature_extractor.get_collate(is_train),
+      n_pos, n_pos, self.pre_tokenize)
+
+  def _get_encoder_pos_bias(self, seq_len, n_image):
+    input_pos_bias = None
+    if self.image_positional_bias in {"zero", "learned"}:
+      first_self_atten = self.model.encoder.block[0].layer[0].SelfAttention
+      if self.image_positional_bias == "zero":
+        input_pos_bias = first_self_atten.compute_bias(seq_len, seq_len)
+        input_pos_bias[:, :, :n_image, :] = 0
+        input_pos_bias[:, :, :n_image] = 0
+      elif self.image_positional_bias.startswith("learned"):
+        n_heads = self.model.config.num_heads
+        n_text = seq_len - n_image
+        i_t_bias = self.learned_image_text_bias.view(1, n_heads, 1, 1).repeat(1, 1, n_image, n_text)
+        t_i_bias = self.learned_text_image_bias.view(1, n_heads, 1, 1).repeat(1, 1, n_text, n_image)
+        i_i_bias = self.learned_image_image_bias.view(1, n_heads, 1, 1).repeat(1, 1, n_image, n_image)
+        t_t_bias = first_self_atten.compute_bias(n_text, n_text)
+        input_pos_bias = torch.cat([
+          torch.cat([i_i_bias, i_t_bias], 3),
+          torch.cat([t_i_bias, t_t_bias], 3)
+        ], 2)
+      else:
+        raise NotImplementedError()
+    elif self.image_positional_bias is not None:
+      raise NotImplementedError(self.image_positional_bias)
+    return input_pos_bias
+
+  def _encode(self, image_inputs, input_ids, input_mask):
+    image: ImageRegionFeatures = self.image_feature_extractor(**image_inputs)
+    device = image.features.device
+
+    if isinstance(self.image_joiner, Linear):
+      detr_embed = self.image_joiner(image.features)
+    else:
+      detr_embed = self.image_joiner(image.features, self.model.shared)
+
+    if self.query_box == "always":
+      # Adds a query embeddings to the query boxes
+      if image.n_boxes is not None:
+        batch_ixs = torch.arange(len(detr_embed), device=device, dtype=torch.long)
+        end_ixs = image.n_boxes - 1
+        detr_embed[batch_ixs, end_ixs] += self.query_embedding
+      else:
+        detr_embed[:, -1] += self.query_embedding
+    elif self.query_box is None:
+      pass
+    else:
+      raise NotImplementedError(self.query_box)
+
+    if self.image_seperator:
+      input_ids = torch.cat([
+        input_ids,
+        torch.full_like(input_ids[:, :1], self._image_sep_id),
+      ], 1)
+      input_mask = torch.cat([input_mask[:, :1], input_mask], 1)
+
+    query_embed = self.model.shared(input_ids)
+
+    query_embed, input_mask = our_utils.concat_masked_sequences(
+      detr_embed, image.n_boxes,
+      query_embed, input_mask
+    )
+
+    if self.query_box == "always":
+      # Remove the query box so our output only contains the object boxes
+      image.n_boxes = image.n_boxes - 1
+      image.boxes = image.boxes[:, :-1]
+      image.features = image.features[:, :-1]
+      image.objectness = image.objectness[:, :-1]
+    elif self.query_box is None:
+      pass
+    else:
+      raise NotImplementedError(self.query_box)
+
+    input_pos_bias = self._get_encoder_pos_bias(input_ids.size(1), image.features.size(1))
+
+    encoder_outputs = self.model.encoder(
+      inputs_embeds=query_embed,
+      attention_mask=input_mask,
+      encoder_positional_bias=input_pos_bias,
+      return_dict=True
+    )
+    return encoder_outputs, input_mask, image
+
+  def _image_rel(self, encoder, image: ImageRegionFeatures):
+    if self.image_relevance is None:
+      n_images = image.boxes.size(1)
+      return self.obj_cls(encoder[:, :n_images])
+    else:
+      rel = self.image_relevance(encoder, image.objectness, image.boxes)
+      if len(rel.size()) == 2:
+        return torch.stack([rel, torch.zeros_like(rel)], -1)
+      else:
+        return rel
+
+  def forward(self, image_inputs, input_ids, input_mask, labels=None,
+              box_targets=None, tasks=None, label_masks=None) -> Tuple[torch.Tensor, Dict[str, float]]:
+
+    encoder_outputs, input_mask, image_features = self._encode(image_inputs, input_ids, input_mask)
+
+    t5_out = self.model(
+      encoder_outputs=encoder_outputs,
+      attention_mask=input_mask,
+      labels=labels,
+      return_dict=True
+    )
+
+    if not self.predict_trailing_pad_tokens:
+      # -100 marks a label as not a target
+      labels = labels.masked_fill(labels == self.tokenizer.pad_token_id, -100)
+
+    rel = self._image_rel(t5_out.encoder_last_hidden_state, image_features)
+
+    boxes = image_features.boxes
+    n_boxes = image_features.n_boxes
+
+    loss, monitor = self.loss(t5_out.logits, labels, boxes,
+                              rel, n_boxes, box_targets, tasks)
+    return loss, monitor
+
+  def predict(
+      self, image_inputs, input_ids, input_mask, labels=None,
+      box_targets=None, tasks=None,
+      allennlp_spec: BeamSearchSpec=None,
+      predict_text=True, mask=None, nms=None
+  ):
+    task = tasks[0]
+    if not all(x == task for x in tasks):
+      raise NotImplementedError("Predict requires all examples have the same batch")
+
+    if mask is not None:
+      buffer_str = f"_{mask.get_name()}_{str(task)}"
+      if not hasattr(self, buffer_str):
+        words = mask.get_target_words(task)
+        voc_len = self.model.config.vocab_size
+        tensor_mask = np.zeros([voc_len], dtype=np.bool)
+        for word in words:
+          tensor_mask[encode_with_cache(word, self.tokenizer, self.tokenizer_cache)] = True
+        tensor_mask[self.tokenizer.eos_token_id] = mask.target_eos()
+        tensor_mask = torch.as_tensor(tensor_mask, device=input_ids.device)
+        self.register_buffer(buffer_str, tensor_mask)
+      tensor_mask = getattr(self, buffer_str)
+      tensor_mask = tensor_mask.float() * mask.val
+    else:
+      tensor_mask = None
+
+    encoder_outputs, input_mask, image_features = self._encode(image_inputs, input_ids, input_mask)
+    rel = self._image_rel(encoder_outputs.last_hidden_state, image_features)
+    rel = rel.softmax(-1)[:, :, 0]
+
+    if predict_text:
+      if tensor_mask is None:
+        post_process = None
+      else:
+        def post_process(logits, _):
+          return F.log_softmax(logits + tensor_mask, -1)
+      bs = allennlp_spec.build(self.tokenizer.eos_token_id)
+      decode_init = t5_initialize_decoding(
+        self.tokenizer, self.model, encoder_outputs[0], input_mask, post_process)
+      input_ids, logprobs = bs.search(*decode_init)
+      input_ids = input_ids.detach().cpu()
+
+      out_text = []
+      for batch in range(len(input_ids)):
+        text = [self.post_process_generation(x) for x in input_ids[batch]]
+        out_text.append(text)
+    else:
+      out_text, logprobs = None, None
+
+    if nms is None:
+      nms = self.nms
+    if nms is not None and nms > 0.0:
+      for i in range(image_features.boxes.size(0)):
+        boxes = image_features.boxes[i]
+        boxes = torchvision.ops.box_convert(boxes, "cxcywh", "xyxy")
+        keep = torchvision.ops.nms(boxes, rel[i], nms)
+        to_subtract = torch.full_like(rel[i], 10000)
+        to_subtract[keep] = 0
+        rel[i] -= to_subtract
+
+    return build_per_example_output(
+      out_text, logprobs, image_features.boxes, rel, image_features.n_boxes)
+
+  def post_process_generation(self, generated_ids):
+    return self.tokenizer.decode(generated_ids, skip_special_tokens=True)
