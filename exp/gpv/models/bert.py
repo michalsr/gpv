6eb443471d2c3335c0798fe01b67e33228d83d96
chrisc <chrisc@allenai.org>
2021-07-27 11:29:08 -0700

    Support cpu and pre-tokenized text in bert.py

--- exp/gpv/models/bert.py
+++ exp/gpv/models/bert.py
@@ -9,14 +9,17 @@ class Bert(nn.Module):
         self.model = BertModel.from_pretrained('bert-base-uncased')
 
     def forward(self,sentences,device=None):
-        token_inputs = self.tokenizer(
-            sentences,
-            padding=True,
-            return_tensors='pt')
-        if device is None:
-            token_inputs = {k:v.cuda() for k,v in token_inputs.items()}
+        if isinstance(sentences, (list, tuple)):
+            token_inputs = self.tokenizer(
+                sentences,
+                padding=True,
+                return_tensors='pt')
+            if device is None:
+                token_inputs = {k:v.cuda() for k,v in token_inputs.items()}
+            else:
+                token_inputs = {k:v.to(device) for k,v in token_inputs.items()}
         else:
-            token_inputs = {k:v.cuda(device) for k,v in token_inputs.items()}
+            token_inputs = sentences
 
         outputs = self.model(**token_inputs)
         return outputs[0], token_inputs # BxTxD (BxTx768)
