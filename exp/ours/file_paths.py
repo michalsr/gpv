3e077da803ac930a406fca1aec11ce92822de6a7
chrisc <chrisc@allenai.org>
2021-08-23 08:52:14 -0700

    Tweak file_paths

--- exp/ours/file_paths.py
+++ exp/ours/file_paths.py
@@ -1,12 +1,13 @@
 from os.path import join, dirname
 
-DATA_DIR = "/home/amitak/gpv-1-web/data/"
+DATA_DIR = "/Users/chris/data/"
 
 FASTER_RCNN_SOURCE = join(DATA_DIR, "faster-rcnn", "trainval_resnet101_faster_rcnn_genome_36.tsv")
 VINVL_SOURCE = join(DATA_DIR, "vinvl")
 VINVL_COCO_FEATURES = join(VINVL_SOURCE, "model_0060000")
 VINVL_MODEL_WEIGHTS = join(VINVL_SOURCE, "models", "vinvl_vg_x152c4.pth")
 
+NOCAPS_VAL_IMAGE_INFO = join(DATA_DIR, "nocaps", "nocaps_val_image_info.json")
 COCO_SOURCE = join(DATA_DIR, "coco")
 COCO_ANNOTATIONS = join(COCO_SOURCE, "annotations")
 VQA2_SOURCE = join(DATA_DIR, "vqa-2.0")
@@ -15,16 +16,18 @@ TORCHVISION_CACHE_DIR = join(DATA_DIR, "torchvision-cache")
 
 WEB_IMAGES_DIR = join(DATA_DIR, "ai2-prior-gpv1web-bing/images")
 
-#CACHE_DIR = join(dirname(dirname(dirname(__file__))), "data-cache")
-CACHE_DIR = "/home/amitak/gpv-2/gpv/data-cache"
+CACHE_DIR = join(dirname(dirname(dirname(__file__))), "data-cache")
+# CACHE_DIR = "/home/amitak/gpv-2/gpv/data-cache"
 PRECOMPUTED_FEATURES_DIR = join(CACHE_DIR, "precomputed-features/coco")
+WEBQA_ALL_FIFTH_ANSWERS = join(CACHE_DIR, "webqa_fifth_answers.json")
 
-GPV_DIR = "/home/amitak/gpv-1-web/"  #join(DATA_DIR, "gpv")
-COCO_IMAGES = join(GPV_DIR, "data/learning_phase_data/coco/images")
+GPV_DIR = join(DATA_DIR, "gpv")
+WEBQA_DIR = join(GPV_DIR, "learning_phase_data/webqa")
+COCO_IMAGES = join(GPV_DIR, "learning_phase_data/coco/images")
 
 PRETRAINED_DETR_MODELS = {
- "coco_sce": join(GPV_DIR, "data/detr", "detr_coco_sce.pth"),
- "coco": join(GPV_DIR, "data/detr", "detr_coco.pth")
+ "coco_sce": join(GPV_DIR, "detr", "detr_coco_sce.pth"),
+ "coco": join(GPV_DIR, "detr", "detr_coco.pth")
 }
 
 OPENSCE_HOME = join(DATA_DIR, "git/opensce")
@@ -33,7 +36,8 @@ OPENSCE_IMAGES = join(OPENSCE_HOME, "images")
 OPENSCE_SYN = join(OPENSCE_HOME, "opensce_synonyms.json")
 OPENSCE_CATS = join(OPENSCE_HOME, "opensce_categories.csv")
 
-SOURCE_DIR = join(GPV_DIR, "data/learning_phase_data")
+SOURCE_DIR = join(GPV_DIR, "learning_phase_data")
+WEBQA80_ANSWERS = join(SOURCE_DIR, "vocab/web_80_answers.json")
 GPV1_VOC = join(SOURCE_DIR, "vocab/vocab.json")
 GPV1_VOC_EMBED = join(SOURCE_DIR, "vocab/vocab_embed.npy")
 
